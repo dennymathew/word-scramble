394814657590ceeb772dba253d5444f99ed12175
Denny Mathew <devdenmathew@gmail.com>
2020-11-21 22:45:28 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Denny Mathew on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
